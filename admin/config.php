3cd5ddb775806d29fe90b3825ebbec65e2abeb0a
taleetarhm <124320571+taleetarhm@users.noreply.github.com>
2023-02-03 11:09:14 +0700

    Create config.php

---
+++ admin/config.php
@@ -0,0 +1,8 @@
+<?php
+$koneksi=mysqli_connect ("localhost", "root", "", "db_prakerin1");
+//cek koneksi
+if (mysqli_connect_error()){
+	echo "Koneksi database gagal :", mysqli_connect_error();
+}else{
+}
+?>
